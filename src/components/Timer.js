d1fa305d745c483316d4a3792db2cc2ab6a4bdc3
David Parsons <info@dpdesigns.eu>
2020-07-16 14:36:28 +0200

    minor change to TimerStyles css file

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -2,7 +2,7 @@ import React, { Fragment } from "react";
 import AnimatedCircle from "./AnimatedCircle";
 // import "../styles/Timer.css";
 import "../styles/OptionsStyles.css";
-import "../styles/Timer.css";
+import "../styles/TimerStyles.css";
 
 const Timer = ({ isActive, timer, time }) => {
   return (
